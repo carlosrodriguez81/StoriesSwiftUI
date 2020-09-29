e1b106cad479fc9a8d8e5ff12b0bf8b141265495
Carlos A Rodriguez <crodri81@gmail.com>
2020-09-29 11:30:23 -0300

    Initial Commit

---
+++ StoriesSwiftUI/StoriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesSwiftUIApp.swift
+//  StoriesSwiftUI
+//
+//  Created by Carlos Rodriguez on 9/29/20.
+//
+
+import SwiftUI
+
+@main
+struct StoriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
